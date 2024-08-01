93170618618fd540b5b25a80c697c5898262617f
Pablo de la Peña <p@shedcollective.org>
2024-08-01 11:15:47 +0100

    chore: Describe fields and allow search by user’s name

--- src/Model/User/Email/Blocker.php
+++ src/Model/User/Email/Blocker.php
@@ -12,7 +12,12 @@
 namespace Nails\Auth\Model\User\Email;
 
 use Nails\Auth\Constants;
+use Nails\Common\Exception\ValidationException;
+use Nails\Common\Helper\Form;
 use Nails\Common\Model\Base;
+use Nails\Email\Resource\Type;
+use Nails\Email\Service\Emailer;
+use Nails\Factory;
 
 /**
  * Class Blocker
@@ -41,4 +46,58 @@ class Blocker extends Base
      * @var string
      */
     const RESOURCE_PROVIDER = Constants::MODULE_SLUG;
+
+    // --------------------------------------------------------------------------
+
+    public function getSearchableColumns(): array
+    {
+        $userModel = Factory::model('User', Constants::MODULE_SLUG);
+        return [
+            sprintf(
+                '(SELECT CONCAT_WS(" ", u.first_name, u.last_name) from `%s` u WHERE u.id = %s.user_id)',
+                $userModel->getTableName(),
+                $this->getTableAlias()
+            ),
+        ];
+    }
+
+    // --------------------------------------------------------------------------
+
+    public function describeFields($sTable = null)
+    {
+        $fields = parent::describeFields($sTable);
+
+        /** @var Emailer $emailer */
+        $emailer = Factory::service('Emailer', \Nails\Email\Constants::MODULE_SLUG);
+
+        $fields['user_id']
+            ->setLabel('User')
+            ->setClass('user-search')
+            ->setIsRequired(true);
+
+        $fields['type']
+            ->setIsRequired(true)
+            ->setType(Form::FIELD_DROPDOWN)
+            ->setClass('select2')
+            ->setPlaceholder('Select a value')
+            ->setOptions(array_merge(
+                [
+                    '' => '',
+                ],
+                array_filter(
+                    array_map(
+                        fn(Type $type) => $type->canUnsubscribe() ? $type->name : null,
+                        $emailer->getTypes()
+                    )
+                )
+            ))
+            ->addValidation(function ($value) use ($emailer) {
+                $type = $emailer->getType($value);
+                if ($type && !$type->canUnsubscribe()) {
+                    throw new ValidationException('It is not possible to unsubscribe from this type of email');
+                }
+            });
+
+        return $fields;
+    }
 }
